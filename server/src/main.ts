c9f50999a18be6a22783d520e2bf192f6563526a
zweroboy1 <fprognoz@gmail.com>
2024-09-28 22:31:35 +0300

    feat: add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
+const configService = new ConfigService();
+
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Events API')
+    .setDescription('API for Elif school test task')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
